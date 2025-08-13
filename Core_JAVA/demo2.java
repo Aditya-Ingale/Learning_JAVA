867d587853e1ef76308b2c00937d1a2f7b1821ee
Aditya-Ingale <adityaa.ingale@gmail.com>
2025-08-14 00:47:00 +0530

    Default and Parameterized Constructor

--- Core_JAVA/demo2.java
+++ Core_JAVA/demo2.java
@@ -1,3 +1,20 @@
+/*
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Default Constructor>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+- A constructor is not exactly a method (it has no return type, not even void), but it runs automatically 
+when an object is created.
+- If we don’t explicitly write one, Java automatically creates a default constructor with no parameters, 
+which assigns default values:
+    - 0 for numbers, false for booleans, null for objects (including String).
+- If we create our own constructor, Java does not add the default one automatically — we must explicitly 
+write it if we still want it.
+- Yes — if you create 2 objects, the constructor runs once for each object.
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Parameterized Constructor>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+- These are constructors with parameters that let you initialize object values at the time of creation, 
+instead of setting them manually later.
+- They provide flexibility to assign different values to different objects when they’re created.
+*/
+
 class Human{
     private String name;
     private int age;
