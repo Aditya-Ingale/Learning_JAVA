34f22fa3a9de22159972bd2662dd87703501bf5c
Aditya-Ingale <adityaa.ingale@gmail.com>
2025-08-26 22:54:58 +0530

    Project-I Quiz Console App

---
+++ Project_I/QuestionService.java
@@ -0,0 +1,11 @@
+package Project_I;
+
+public class QuestionService {
+
+    Question[] obj = new Question[5];
+
+    public void displayQuestions(){
+        System.out.println("Displaying Questions");
+    }
+
+}
